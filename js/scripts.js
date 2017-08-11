c9261b1f63ae2ac368f0e14f9bc805058df46063
Elrey Belmonti <elbelmonti@gmail.com>
2017-08-11 11:44:23 -0700

    add jquery selector to and logic to show one panel result

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $(".track-form").submit(function(event){
+    event.preventDefault();
+
+    var sizeInput = parseInt($("#size").val());
+    var photoInput = parseInt($("input:radio[name=photo]:checked").val());
+    var interestInput = parseInt($("#interest").val());
+    var logoInput = parseInt($("input:radio[name=logo]:checked").val());
+    var codeInput = parseInt($("#code").val());
+
+    // $("#design, #ruby, #c, #track-form").hide();
+
+
+    var result = sizeInput + photoInput + interestInput + logoInput + codeInput;
+    console.log(sizeInput);
+    console.log(photoInput);
+    console.log(interestInput);
+    console.log(logoInput);
+    console.log(codeInput);
+    console.log(result);
+
+    if (result <= 5){
+      $("#c, .panel").show();
+    }
+// for debugging
+    // console.log(sizeInput);
+    // console.log(photoInput);
+    // console.log(interestInput);
+    // console.log(logoInput);
+    // console.log(codeInput);
+    // console.log(result);
+
+  });
+
+
+// to return to survey
+  // $("#return").click(function(){
+  //   $("#uhura, #scotty, #spock, #kirk, .panel").hide();
+  //   $(".quiz").show();
+  // });
+});
